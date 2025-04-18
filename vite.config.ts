70ba9e730e6177fc86de1e4f07d526e82fc441bb
kkase-labellevie <kkase@labellevie.jp>
2025-04-18 18:25:45 +0900

    feat: サンプルコード追記

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 export default defineConfig({
   plugins: [vanillaExtractPlugin(), sveltekit()],
-});
+  build: {
+    //cssCodeSplit: false, // CSSを1ファイルにまとめる設定
+  },
+})
