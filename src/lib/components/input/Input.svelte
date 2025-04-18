6dd822ac724bc70fb4f7bbb5ec8529846feb7dee
kkase-labellevie <kkase@labellevie.jp>
2025-04-18 15:42:07 +0900

    fix: 不要なファイルを削除

--- src/lib/components/input/Input.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import { input } from '../../styles/input/input.css'
-  export let type: string = 'text'
-  export let placeholder: string = ''
-  export let bindValue: string = ''
-</script>
-
-<input {type} {placeholder} bind:value={bindValue} class={input} />
